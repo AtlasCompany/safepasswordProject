f78e12789b528faa10e1ea04d63bc04893621ca1
AtlasCompany <ar.vahedi357@gamil.com>
2014-05-31 21:57:53 +0430

    Delete explain.js

--- web/js/explain.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $("#click").click(function () {
-        $("#whatIsThis").hide("fast", function () {
-            $("#explain").slideDown("10000");
-        });
-    });
-});
